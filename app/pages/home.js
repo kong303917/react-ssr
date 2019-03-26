e4049f07f9ca6df105f0494d43199897fe29b762
nick.zhou <nick.zhou@landi.com>
2019-03-26 10:49:09 +0800

    init

---
+++ app/pages/home.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Home = props => (
+    <div>
+        <h1>{ props.title }</h1>
+        <Link to="/list">跳转列表页</Link>
+    </div>
+)
+
+function mapStateTpProps(state) {
+    return { ...state.home }
+}
+
+export default connect(mapStateTpProps)(Home)
